74bfd5a24e2e2668fb074c34f695bae837b85104
Jean Demel <demeljean@hotmail.fr>
2024-07-22 16:16:48 +0200

    delete button

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Button, Pressable, Text, TextInput, View } from "react-native";
 
 
 
@@ -7,14 +7,35 @@ export default function TodoList() {
     const [task, setTask] = useState('Default value');
     const [list, setList] = useState(['Test', 'Toust', 'Toast']);
 
+    function addTask() {
+        setList([
+            ...list,
+            task
+        ]);
+    }
+
+    function removeTask(indexToRemove) {
+        setList(
+            list.filter((item, index) => index !== indexToRemove)
+        );
+    }
+
     return (
         <View>
-            <TextInput 
-                placeholder="Enter a task" 
-                value={task} 
+            <TextInput
+                placeholder="Enter a task"
+                value={task}
                 onChangeText={(text) => setTask(text)} />
+            <Button title="Add task" onPress={addTask} />
             <Text>You are typing : {task}</Text>
-            {list.map((item, index) => <Text key={index}>{item}</Text>)}
+            {list.map((item, index) =>
+                <View key={index} style={{flexDirection:"row", alignItems:'center', justifyContent:'space-between', margin:2}}>
+                    <Text>{item}</Text>
+                    <Button title="X" onPress={() => removeTask(index)} />
+                    {/* <Pressable style={{padding:10, backgroundColor: 'red', borderRadius:10}} onPress={() => removeTask(index)}>
+                        <Text>X</Text>
+                    </Pressable> */}
+                </View>)}
         </View>
     );
 }
