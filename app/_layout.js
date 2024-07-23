012cbd48c8f9b4239e4b151fd271f2e1986ace58
Jean Demel <demeljean@hotmail.fr>
2024-07-23 14:32:45 +0200

    comment and small tweaks

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,12 @@
 import { Slot } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-
+/**
+ * Le fichier _layout sera chargé automatiquement par le expo router pour toutes les pages.
+ * Il permet de définir une structure commune à chaque page, le Slot représentant le 
+ * contenu de la page sur laquelle on se trouve. Ici on s'en sert pour faire que toutes
+ * les pages puissent avoir des Gesture
+ */
 export default function Layout() {
     return (
         <GestureHandlerRootView>
--- app/gesture.js
+++ app/gesture.js
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Directions, Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
@@ -7,43 +8,45 @@ export default function gesture() {
     const [leftPos, setLeftPos] = useState(100);
     const [topPos, setTopPos] = useState(200);
     const flingRight = Gesture.Fling()
-    .direction(Directions.RIGHT)
-    .onStart(event => setLeftPos(leftPos+25));
+        .direction(Directions.RIGHT)
+        .onStart(event => setLeftPos(leftPos + 25));
 
     const flingLeft = Gesture.Fling()
-    .direction(Directions.LEFT)
-    .onStart(event => setLeftPos(leftPos-25));
+        .direction(Directions.LEFT)
+        .onStart(event => setLeftPos(leftPos - 25));
 
     const flingUp = Gesture.Fling()
-    .direction(Directions.UP)
-    .onStart(event => setTopPos(topPos-25));
+        .direction(Directions.UP)
+        .onStart(event => setTopPos(topPos - 25));
 
     const flingDown = Gesture.Fling()
-    .direction(Directions.DOWN)
-    .onStart(event =>  setTopPos(topPos+25));
+        .direction(Directions.DOWN)
+        .onStart(event => setTopPos(topPos + 25));
 
     return (
-        
-            <GestureDetector gesture={Gesture.Race(flingLeft,flingRight, flingUp, flingDown)}>
-                <View style={styles.container}>
-                    <View style={{...styles.square, left: leftPos, top: topPos}}></View>
-                </View>
+        <View style={styles.container}>
+            <Link href="/">Go Back</Link>
+            <GestureDetector gesture={Gesture.Race(flingLeft, flingRight, flingUp, flingDown)}>
+            
+                    <View style={{ ...styles.square, left: leftPos, top: topPos }}></View>
+            
             </GestureDetector>
+        </View>
 
-        
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor:'white',
-        flex:1
+        backgroundColor: 'white',
+        flex: 1,
+        paddingTop:50
     },
     square: {
         backgroundColor: 'red',
         width: 100,
         height: 100,
-        position:'absolute',
+        position: 'absolute',
 
     }
 })
