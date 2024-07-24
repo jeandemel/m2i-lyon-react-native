4862a34c7cf8c746d0d5516880c11b29a867a069
Jean Demel <demeljean@hotmail.fr>
2024-07-24 12:33:42 +0200

    infinite scroll, scroll to top in progress

--- app/data-dog.js
+++ app/data-dog.js
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Button, FlatList, Pressable, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ItemDog from "../components/ItemDog";
 
 
 export default function dataDog() {
     const [dogList, setDogList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const list = useRef();
     /**
      * Le useEffect contient une fonction qui se lance au chargement du component puis
      * à chaque fois que la ou les variables du tableau en deuxième arguments changent,
@@ -15,31 +18,55 @@ export default function dataDog() {
      * un appel serveur
      */
     useEffect(() => {
-        
+
 
         fetchData();
-    }, []);
+    }, [page]);
 
     async function fetchData() {
-        setIsLoading(true);
-        const response = await axios.get('https://bunny-relaxing-quickly.ngrok-free.app/api/dog', {
-            'ngrok-skip-browser-warning': 'true' //Nécessaire uniquement pasqu'on utilise ngrok pour "héberger" le serveur, dans 99.99% des cas, on a pas besoin de ça
-        });
-        setDogList(response.data);
-        setIsLoading(false);
+        try {
+
+            setIsLoading(true);
+            const response = await axios.get('https://bunny-relaxing-quickly.ngrok-free.app/api/dog', {
+                headers: {
+                    'ngrok-skip-browser-warning': 'true' //Nécessaire uniquement pasqu'on utilise ngrok pour "héberger" le serveur, dans 99.99% des cas, on a pas besoin de ça
+                },
+                params: { page }
+            });
+            setDogList([
+                ...dogList,
+                ...response.data
+            ]);
+            setIsLoading(false);
+        }catch(error) {
+            alert(error);
+        }
+
     }
 
+    function refreshHandle(){
+        setPage(1);
+        setDogList([]);
+        fetchData();
+    }
     return (
         <SafeAreaView style={styles.container}>
-            <Text>The Dog List</Text>
-
+            <Text>The Dog List, current page = {page}</Text>
+            {/* <Button onPress={() => setPage(page + 1)} title="Next Page" /> */}
+            <Pressable onPress={() => list.scrollToIndex(0)} style={styles.floatingButton}>
+                <Text style={{color:'white'}}>⤊</Text>
+            </Pressable>
             <FlatList
+                ref={list}
+                style={{ width: '100%' }}
                 refreshing={isLoading}
-                onRefresh={() => fetchData()}
+                onRefresh={refreshHandle}
+                onEndReached={() => setPage(page+1)}
+                // onEndReachedThreshold={3}
                 data={dogList}
                 keyExtractor={item => item.id}
                 renderItem={({ item }) => (
-                    <Text>{item.name}</Text>
+                    <ItemDog dog={item} />
                 )}
             />
         </SafeAreaView>
@@ -50,5 +77,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center'
+    },
+    floatingButton: {
+        padding:20,
+        borderRadius:100,
+        backgroundColor: 'skyblue',
+        fontSize: 20,
+        fontWeight: 'bold',
+        position: 'absolute',
+        bottom: 10,
+        right: 10
     }
 })
---
+++ components/ItemDog.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+/**
+ * Component représentant un chien dans une liste de chien
+ * @param {Dog} dog Le chien à afficher
+ */
+export default function ItemDog({dog}) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{dog.name}</Text>
+            <Text style={styles.subTitle}>{dog.breed}</Text>
+            <Text style={styles.detail}>{new Date(dog.birthdate).toLocaleDateString()}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 20,
+        margin: 10,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: 'lightgray'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    subTitle: {
+        fontSize: 15
+    },
+    detail: {
+        textAlign:'right',
+        color: 'darkgray'
+    }
+})
