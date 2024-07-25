38251ed716a74118b2dac6441ef0625734db66c2
Jean Demel <demeljean@hotmail.fr>
2024-07-25 15:27:40 +0200

    random x position and rotation

--- app/game.js
+++ app/game.js
@@ -1,17 +1,26 @@
 import { useEffect } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { View } from "react-native";
 import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
 
 
 export default function game() {
     const top = useSharedValue(0);
-
+    const rotation = useSharedValue(0);
+    const left = useSharedValue(0);
+    
     const anitmationStyle =  useAnimatedStyle(() => ({
-        top: top.value+'%'
+        top: top.value+'%',
+        left: left.value+'%',
+        transform:[
+            {rotate: rotation.value+'deg'}
+        ]
     }))
 
     useEffect(() => {
-        top.value = withRepeat(withTiming(100, {duration:3000,easing: Easing.linear}), -1);
+        top.value = withRepeat(withTiming(100, {duration:3000,easing: Easing.linear}, () => {
+            left.value = Math.floor(Math.random() * 85);
+        }), -1);
+        rotation.value = withRepeat(withTiming(360, {duration:1000,easing: Easing.linear}), -1);
     }, []);
 
 
